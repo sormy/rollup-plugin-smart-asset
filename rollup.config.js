4a8f21c70378cb550880e8f6018e9702f429008b
Artem Butusov <butusova@amazon.com>
2019-03-03 15:53:49 -0500

    Add missing external

--- rollup.config.js
+++ rollup.config.js
@@ -3,7 +3,7 @@ import babel from "rollup-plugin-babel"
 export default {
   input: "src/index.js",
   plugins: [ babel() ],
-  external: [ "util", "fs", "path", "mkdirp", "asset-hash", "mime" ],
+  external: [ "util", "fs", "path", "mkdirp", "asset-hash", "mime", "magic-string" ],
   output: [
     { file: "dist/rollup-plugin-smart-asset.cjs.js", format: "cjs" },
     { file: "dist/rollup-plugin-smart-asset.esm.js", format: "esm" },
