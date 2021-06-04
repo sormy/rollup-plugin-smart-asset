849a690c193f2bb8c64323c35324d4a0c4ea7550
Broscaru Sorin Mihai <71869645+visma-sorinbroscaru@users.noreply.github.com>
2021-06-04 15:50:09 +0300

    normalize slashes for the assetName when mode is rebase

--- src/smartAsset.js
+++ src/smartAsset.js
@@ -246,7 +246,7 @@ export default (initialOptions = {}) => {
           return buildExportDefaultCode(newAssetPath, idComment)
         }
         case "rebase": {
-          const assetName = relative(options.rebasePath, id)
+          const assetName = normalizeSlashes(relative(options.rebasePath, id));
           const newAssetPath = getAssetPublicPath(assetName, options.publicPath)
           return buildExportDefaultCode(newAssetPath, idComment)
         }
